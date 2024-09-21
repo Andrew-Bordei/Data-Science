9f57557d461c572ff26d91ac4a61fa8ae8a6b46c
Andrew Bordei <113117706+Andrew-Bordei@users.noreply.github.com>
2024-09-21 12:22:07 -0500

    Cleaning file's documentation

--- oil_model/utils/cleaning.py
+++ oil_model/utils/cleaning.py
@@ -2,6 +2,7 @@ import pandas as pd
 from fredapi import Fred
 import yfinance as yf 
 from functools import reduce
+from datetime import datetime
 
 fred_key = '3932ed65675a268ca1a4e0e68b10fa9b'
 fred = Fred(api_key=fred_key)
@@ -24,34 +25,70 @@ financial_api_keys = {
     "usd":"DX=F",
 }
 
-def merge_df(df):
-    merged_df = reduce(lambda left,right: pd.merge(left,right,on=['Date'], how='outer'),df)
+def merge_df(df_list: list[pd.DataFrame]) -> pd.DataFrame:
+    """Will return a dataframe that has been merged from list of dataframes  
+        
+    Args:
+        df (list): List of pd.DataFrames that have been cleaned 
+
+    Returns:
+        pd.DataFrames: merged dataframe 
+    """
+    merged_df = reduce(lambda left,right: pd.merge(left,right,on=['Date'], how='outer'),df_list)
 
     return merged_df
 
-def get_oil_data(key):
+def get_oil_data(dict: dict()) -> pd.DataFrame:
+    """Downloads oil production data from FRED API, saves each df to a list 
+        
+    Args:
+        dict dict(): Dictionary with countries and dataset key     
+    Returns:
+        pd.DataFrame: List of dataframes with oil production data  
+    """
     dataframes = []
-    for i in key: 
-        df = pd.DataFrame({f"{i}_oil_production":fred.get_series(f'{key[i]}')})
+    for key in dict: 
+        df = pd.DataFrame({f"{key}_oil_production":fred.get_series(f'{dict[key]}')})
         dataframes.append(df)
 
     return dataframes
 
-def get_financial_data(key,start,end):
+def get_financial_data(dict: dict(), start: datetime, end: datetime) -> pd.DataFrame: 
+    """Doanloads financial data frome Yahoo Finance API, saves each df to a list 
+        
+    Args:
+        dict dict(), start datetime, end datetime: Dictionary with dataset keys to download, start & end date  
+    Returns:
+        pd.DataFrame: List of dataframes with financial data  
+    """
     dataframes = []
-    for i in key: 
-        df = pd.DataFrame({f"{i}":yf.download(f"{key[i]}", start=start, end=end)['Adj Close']})
+    for key in dict: 
+        df = pd.DataFrame({f"{key}":yf.download(f"{dict[key]}", start=start, end=end)['Adj Close']})
         dataframes.append(df)
 
     return dataframes
 
-def axis_manipulation(df):
+def axis_manipulation(df: pd.DataFrame) -> pd.DataFrame:
+    """Renaming axis to be consistent  
+        
+    Args:
+        df pd.DataFrame: dataframe  
+    Returns:
+        pd.DataFrame: dataframe  
+    """
     for i in df:
         i.rename_axis(index="Date",inplace=True)
     
     return df
 
-def resample_data(df):
+def resample_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Changes the data sampling from monthly, yearly to daily. Filling the nan values with respective row entry  
+        
+    Args:
+        df pd.DataFrame: dataframe 
+    Returns:
+        pd.DataFrame: dataframe 
+    """
     for i in range(len(df)):
         df[i] = df[i].resample('D').ffill()
     

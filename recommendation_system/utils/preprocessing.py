76e6db596f489e6a6dedd05f2b193c3352c08a31
Andrew Bordei <113117706+Andrew-Bordei@users.noreply.github.com>
2024-11-11 09:21:37 -0600

    Preprocessing pipeline

--- recommendation_system/utils/preprocessing.py
+++ recommendation_system/utils/preprocessing.py
@@ -1,10 +1,76 @@
-def encoding(df:pl.DataFrame) -> pl.DataFrame: 
-    encoding = df_cleaned.filter(pl.col('country') == "United States").select(
-    pl.col('fullVisitorId'),pl.col('country'), pl.col('browser'),
-    pl.col('transactionId'),pl.col('v2ProductName')
-    )
+import polars as pl
+from sklearn.preprocessing import LabelEncoder
+import numpy as np 
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def encoding(df: pl.DataFrame) -> pl.DataFrame: 
+    """ 
+    Args:
+         : 
+         :   
+        
+    Returns:
+        pl.DataFrame:  
+    """
+    df = df.select(pl.col('fullVisitorId'),pl.col('country'), 
+              pl.col('browser'),pl.col('transactionId'),
+              pl.col('v2ProductName'))
     
-    canada = df_cleaned.filter(pl.col('country') == "Canada").select(
-    pl.col('fullVisitorId'),pl.col('country'), pl.col('browser'),
-    pl.col('transactionId'),pl.col('v2ProductName')
-    )
+    df = df.with_columns(pl.when(pl.col('transactionId') != 'null').then(1).
+                         otherwise(0).alias("transactionId"))
+    
+    purchases_df = df.filter(pl.col('transactionId') == 1)
+
+    encoder = LabelEncoder()
+
+    for i in purchases_df.columns:
+        if i == 'transactionId':
+            continue
+        encoder.fit(purchases_df.select(pl.col(f'{i}')))
+        encoded_data = encoder.transform(purchases_df.select(pl.col(f'{i}')))
+        purchases_df = purchases_df.with_columns(pl.Series(f'{i}', encoded_data))
+    
+    return purchases_df
+
+def calculate_similarity(df: pl.DataFrame) -> dict[str,int]:
+    """ 
+    Args:
+        : 
+        :   
+        
+    Returns:
+        pl.DataFrame:  
+    """
+    profile_avg_similarity = {}
+    for i in range(0,len(df)):
+        user_profile_df = np.vstack(user_profiles_no_id[i]).T
+        avg_similarity = cosine_similarity(user_profile_df, [active_users[-1]])
+        avg_similarity = avg_similarity.mean()
+        profile_avg_similarity[f'{user_profile_df['fullVisitorId'][i]}'] = avg_similarity
+    return profile_avg_similarity
+
+def sort_similarities(profile_avg_similarity: dict) -> dict[str,int]:
+    """ 
+    Args:
+        : 
+        :   
+        
+    Returns:
+        pl.DataFrame:  
+    """
+    sorted_dict = sorted(profile_avg_similarity.items(), key=lambda x: x[1])
+    return sorted_dict
+
+def recommendation(df: pl.DataFrame, similarity_dict: dict) -> pl.DataFrame: 
+    """ 
+    Args:
+        : 
+        :   
+        
+    Returns:
+        pl.DataFrame:  
+    """
+    recommendations = df.filter((pl.col('fullVisitorId') == similarity_dict[-1][0]) & ((pl.col('transactionId') != "null")))
+    recommendations = recommendations.select(pl.col('v2ProductName')).unique()
+    return recommendations

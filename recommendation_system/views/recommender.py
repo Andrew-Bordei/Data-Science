c217fc566fb3a456f3d9b8d1c2dfaaf00be1ab75
Andrew Bordei <113117706+Andrew-Bordei@users.noreply.github.com>
2024-11-30 10:59:33 -0600

    Main page styling

--- recommendation_system/views/recommender.py
+++ recommendation_system/views/recommender.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import polars as pl
 import plotly.express as px 
+import plotly.graph_objects as go
 from utils import preprocessing
 import os 
+
+import pandas as pd 
 
 path = os.path.join(os.path.dirname(__file__), "..", "data",)
 path2 = os.path.join(os.path.dirname(__file__), "..", "assets",)
@@ -20,39 +23,35 @@ purchasing_users=df.filter(pl.col('transactionId') != "null").select(pl.col('ful
 purchasing_users=purchasing_users.filter(pl.col('fullVisitorId') != "None")
 user_profiles = preprocessing.user_profiles(encoded_df)
 
-img = '''
-<style>
-body {
-backgroud-image: url("https://unsplash.com/photos/body-of-water-during-sunset-a2ntpw5HJgg");
-background-size: cover;
-}
-</style>
-''' 
-st.markdown(img, unsafe_allow_html=True)
-
 st.title("Ecommerce Recommender")
 st.subheader("An App by Andrew Bordei")
 st.write(
     """
-    This app provides purchase recommendations to visitors of the Google Merch website. Recommendations are determined by finding
-    the most similar previous purchasing users compared to the current user, then recommending past user purchases to the current user. 
-    Similarity among users is determined by Cosine similarity. The data used is real traffic from the 
-    Google store, which can be found [here](https://www.kaggle.com/datasets/bigquery/google-analytics-sample/data). 
-    The entire code for the app is available on my [GitHub](https://github.com/Andrew-Bordei/Data-Science/tree/main/recommendation_system).
-    If you have any questions, contact me on [LinkedIn](https://www.linkedin.com/in/andrew-bordei-80448813b/).
+    Select a visitor from the Google Merch website below to provide them with purchase recommendations! If you have any questions, 
+    contact me on [LinkedIn](https://www.linkedin.com/in/andrew-bordei-80448813b/).
     """
 )
 st.write("\n")
-
 left_column, right_column = st.columns(2)
 with left_column:
-    "Select a user to recommend products to:"
-    user_selected = st.selectbox(options=purchasing_users, label="User ID's")
+    "Select a visitor to recommend products to:"
+    st.write("\n")
+    st.write("\n")
+    user_selected = st.selectbox(options=purchasing_users, label="Visitor ID's")
     
 with right_column:
-    "Recommendations!"
+    # "Recommendations!"
     recommendations = preprocessing.recommendation_pipeline(user_selected, user_profiles,encoded_df,df)
-    st.dataframe(recommendations,width=200,column_config={"value":"Recommendations"})
+    fig = go.Figure(data=[go.Table(header=dict(values=["Recommendations"],line_color='black',fill_color='skyblue',
+                                               align='center',
+                                               font=dict(color='black', size=14),
+                                               height=30),
+                                   cells=dict(values=[recommendations],line_color='black',fill_color='lightblue',
+                                              align='center',
+                                              font=dict(color='black', size=14),
+                                              height=30))])
+    st.write(fig)
+    # st.dataframe(recommendations,width=200,column_config={"value":"Recommendations"})
 
 model_eval = preprocessing.recommendation_eval_pipeline(user_selected, user_profiles,encoded_df,df)
 
@@ -60,6 +59,6 @@ model_accuracy=model_accuracy.with_columns(model_eval)
 model_accuracy=model_accuracy.transpose()
 names = names.with_columns(model_accuracy)
 names = names.rename({"column_0":"Accuracy"})
-visitors_px = px.bar(names, x='Data',y='Accuracy', title="Recommendation Accuracy")
-st.plotly_chart(visitors_px)
+visitors_px = px.bar(names, x='Data',y='Accuracy', title="Recommendation Accuracy", color='Data')
+st.plotly_chart(visitors_px) 
     
